5a77c7fec8386422759c678a72b99c0ed9110dca
Taylor Jordan <jordatay@oregonstate.edu>
2023-05-18 00:00:24 -0700

    Skeleton for main pages linked by nav complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,18 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from "./components/nav";
 import HomePage from "./pages/HomePage";
+import ParksPage from "./pages/ParksPage";
+import FacilitiesPage from "./pages/FacilitiesPage";
+import BiologicalAssetsPage from "./pages/BiologicalAssetsPage";
+import EmployeesPage from "./pages/EmployeesPage";
+import TasksAssignedPage from "./pages/TasksAssignedPage";
+import EmployeeTasksPage from "./pages/EmployeeTasksPage";
 import TaskCategoriesPage from "./pages/TaskCategoriesPage";
+import SpeciesPage from "./pages/SpeciesPage";
+import DietsPage from "./pages/DietsPage";
+import HabitatsPage from "./pages/HabitatsPage";
+import FacilityTypesPage from "./pages/FacilitiesPage";
+import JobClassificationsPage from "./pages/JobClassificationsPage";
 
 // Establish common variable for host for easy edits
 const hostURL = 'http://localhost:3001';
@@ -31,7 +42,18 @@ function App() {
             {/* Load different page content here depending on route below */}
             <Routes>
                 <Route path="/" element={<HomePage hostURL={hostURL} />} />
+                <Route path="/Parks" element={<ParksPage hostURL={hostURL} />} />
+                <Route path="/Facilities" element={<FacilitiesPage hostURL={hostURL} />} />
+                <Route path="/BiologicalAssets" element={<BiologicalAssetsPage hostURL={hostURL} />} />
+                <Route path="/Employees" element={<EmployeesPage hostURL={hostURL} />} />
+                <Route path="/TasksAssigned" element={<TasksAssignedPage hostURL={hostURL} />} />
+                <Route path="/EmployeeTasks" element={<EmployeeTasksPage hostURL={hostURL} />} />
                 <Route path="/TaskCategories" element={<TaskCategoriesPage hostURL={hostURL} />} />
+                <Route path="/Species" element={<SpeciesPage hostURL={hostURL} />} />
+                <Route path="/Diets" element={<DietsPage hostURL={hostURL} />} />
+                <Route path="/Habitats" element={<HabitatsPage hostURL={hostURL} />} />
+                <Route path="/FacilityTypes" element={<FacilityTypesPage hostURL={hostURL} />} />
+                <Route path="/JobClassifications" element={<JobClassificationsPage hostURL={hostURL} />} />
             </Routes>
         </section>
       </main>
