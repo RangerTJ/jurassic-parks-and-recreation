e4b6f5f855ed1f614bec2ac4ec17adf1bab83632
Taylor Jordan <jordatay@oregonstate.edu>
2023-05-18 01:47:01 -0700

    housekeeping for nav bar

--- frontend/src/components/nav.js
+++ frontend/src/components/nav.js
@@ -10,16 +10,16 @@ function Nav() { return (
         <Link to="/" >Home</Link>
         <Link to="/Parks" >Parks</Link>
         <Link to="/Facilities" >Facilities</Link>
-        <Link to="/BiologicalAssets" >Biological Assets</Link>
+        <Link to="/FacilityTypes" >Facility Types</Link>
         <Link to="/Employees" >Employees</Link>
+        <Link to="/JobClassifications" >Job Classifications</Link>
         <Link to="/TasksAssigned" >Tasks Assigned</Link>
         <Link to="/EmployeeTasks" >Employee Tasks</Link>
         <Link to="/TaskCategories" >Task Categories</Link>
+        <Link to="/BiologicalAssets" >Biological Assets</Link>
         <Link to="/Species" >Species</Link>
         <Link to="/Diets" >Diets</Link>
         <Link to="/Habitats" >Habitats</Link>
-        <Link to="/FacilityTypes" >Facility Types</Link>
-        <Link to="/JobClassifications" >Job Classifications</Link>
       </nav>
     </>
     );
