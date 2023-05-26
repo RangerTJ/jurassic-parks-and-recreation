2c7d9b3c36019f4146f9d570c3a891f1a907c8e6
Taylor Jordan <jordatay@oregonstate.edu>
2023-05-26 01:02:05 -0700

    Files actually added

---
+++ frontend/src/forms/TaskCategoriesUpdateForm.js
@@ -0,0 +1,81 @@
+// Basic CRUD operations and React implementation was heavily based on the CRUD React tutorial series created by PedroTech
+// URLs - Part1: https://www.youtube.com/watch?v=T8mqZZ0r-RA, Part2: https://www.youtube.com/watch?v=3YrOOia3-mo, Part3: https://www.youtube.com/watch?v=_S2GKnFpdtE
+
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import Axios from 'axios';
+
+
+// HostURL Passed from App.js
+function TaskCategoriesUpdateForm ({hostURL}) {
+
+    // Follows reference strategy to read state object, as suggested by stackoverflow user Abdulazeez Jimoh on 10/25/2022
+    // URL: https://stackoverflow.com/questions/68911432/how-to-pass-parameters-with-react-router-dom-version-6-usenavigate-and-typescrip
+    const location = useLocation();
+    const { id, oldCategoryName} = location.state;
+
+    // Facility Type SQL Endpoint
+    const updateTaskCategoriesURL = hostURL + '/api/updateTaskCategories';
+    const navTo = useNavigate();
+
+    // Facility Type States for the Form
+    const [categoryName, setCategoryName] = useState('')
+
+    // Pre-sets all the old values into the fields
+    useEffect(()=> {
+        setCategoryName(oldCategoryName);
+    }, [])
+
+    // UPDATE - Submit Changes to a Bio Asset then return to Asset home (hours/cost can be zero'd in case they need to be cleared for an entry error)
+    const update = async () => {
+        try {
+            if (categoryName) {
+                await Axios.put(updateTaskCategoriesURL, {
+                    categoryName: categoryName,
+                    idTaskCategory: id,
+                });
+                alert(`${categoryName}'s database record has been updated!`)
+                navTo('/TaskCategories');
+                } else {
+                    alert("Please fill out all required fields and try again.")
+                }
+        } catch (error) {
+                console.error('Error updating Category.', error)
+        };
+    };
+
+    return (
+        <>
+            <h2>Update Task Category</h2>
+            <article>
+                <p>
+                    If you would like to update this entry, enter new values for its attributes below
+                    and click the "Save" button.
+                </p>
+                <form>
+                    <fieldset>
+                        <legend>Information</legend>
+                            <div className="selectorP">
+                            <div><label htmlFor="categoryName">Name</label></div>
+                                <input 
+                                    type="text"
+                                    id="categoryName"
+                                    name="categoryName"
+                                    placeholder="Ex. IT, Medical, etc." 
+                                    required
+                                    value={categoryName}
+                                    autoFocus
+                                    onChange={(e) => {setCategoryName(e.target.value)}
+                                    }/>
+                            </div>
+                    </fieldset>
+                </form>
+                <div>
+                    <p><button onClick={update}>Save</button></p>
+                </div>
+            </article>
+        </>
+    );
+}
+
+export default TaskCategoriesUpdateForm;
